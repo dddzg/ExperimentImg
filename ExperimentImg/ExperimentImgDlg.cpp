f5a3d97e7001fd0aeb2b5448305de18b8fb3a3ef
dddzg <240936337@qq.com>
2017-11-03 22:09:30 +0800

    fix: 融合时透明度

--- ExperimentImg/ExperimentImgDlg.cpp
+++ ExperimentImg/ExperimentImgDlg.cpp
@@ -283,7 +283,7 @@ void CExperimentImgDlg::OnBnClickedButton2()
 		this->LogInfo("处理中");
 		auto initTime = clock();
 		if (typeName == "融合左右图") {
-			this->m_pImgDst=ImageProcesser::merge(m_pImgSrc, m_pImgDst, 0.5);
+			this->m_pImgDst=ImageProcesser::merge(m_pImgSrc, m_pImgDst, double(threadNum)/10);
 		}else {
 			ImageProcesser imgPro(this->m_pImgSrc, typeName, threadNum, isCurrent);
 			this->m_pImgDst = imgPro.go();
